acb08ddc6f7cfb25a20b576a529a6d2ebdbdd702
loganbalck <51034430+loganbalck@users.noreply.github.com>
2021-01-23 00:40:54 -0800

    second commit

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,5 +1,5 @@
+import moment from "moment";
 import React, { Component } from "react";
-
 import PortfolioItem from "./portfolio-item";
 
 
@@ -7,15 +7,50 @@ export default class PortfolioContainer extends Component {
     constructor() {
         super();
 
-        console.log("Portfolio container has rendered");
-    }
+    this.state = {
+        pageTitle: "Welcome to my portfolio",
+        isLoading: false,
+        data: [
+            {title: "Quip", category: "eCommerce" }, 
+            {title: "Eventbrite", category: "Scheduling" },
+            {title: "Ministry Safe", category: "Enterprise" },
+            {title: "SwingAway", category: "eCommerce" }
+        ]
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+}
+
+handleFilter(filter) {
+    this.setState({ 
+        data: this.state.data.filter(item => {
+            return item.category === filter;
+        })
+    })
+}
+
+portfolioItems() {
+    return this.state.data.map(item => {
+        return <PortfolioItem title={item.title} url={"goole.com"} />;
+    });
+}
+
+
 
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
-                <h2>Portfolio items go her updated....</h2>
-                <h1>Helloooooooo</h1>
-                <PortfolioItem />
+                <h2>{this.state.pageTitle}</h2>
+
+                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
+                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
+                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
+
+                {this.portfolioItems()}            
             </div>
         )
     }
