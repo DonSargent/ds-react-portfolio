dcfbf9478dc97f5608f97633ea0af84710ab3a30
loganbalck <51034430+loganbalck@users.noreply.github.com>
2021-01-25 14:51:16 -0800

    second commit

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -42,7 +42,9 @@ getPortfolioItems() {
 portfolioItems() {
     return this.state.data.map(item => {
         console.log("item data", item);
-        return <PortfolioItem title={item.name} url={item.url} slug={item.id} />;
+        return <PortfolioItem 
+            key={item.id} title={item.name} 
+            url={item.url} slug={item.id} />;
     });
 }
 
