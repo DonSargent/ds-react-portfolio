6c93fffbf481c5f3b8d5199c2ad1414e65e63d5f
loganbalck <51034430+loganbalck@users.noreply.github.com>
2021-02-01 23:25:08 -0800

    second commit

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -142,7 +142,7 @@ export default class PortfolioForm extends Component {
       
 
         <form onSubmit={this.handleSubmit}  className="portfolio-form-wrapper">
-          <div>
+          <div className="two-column">
             <input
               type="text"
               name="name"
@@ -160,28 +160,28 @@ export default class PortfolioForm extends Component {
             />
           </div>
 
-            <div>
-                <input
-                    type="text"
-                    name="position"
-                    placeholder="Position"
-                    value={this.state.position}
-                    onChange={this.handleChange}
-                />
+            <div className="two-column">
+              <input
+                  type="text"
+                  name="position"
+                  placeholder="Position"
+                  value={this.state.position}
+                  onChange={this.handleChange}
+              />
 
-                <select
-                    name="category"
-                    value={this.state.category}
-                    onChange={this.handleChange}
-                    className="select-element"
-                >
-                    <option value="Ecommerce">Ecommerce</option>
-                    <option value="Scheduling">Scheduling</option>
-                    <option value="Enterprise">Enterprise</option>
-                </select>
+              <select
+                  name="category"
+                  value={this.state.category}
+                  onChange={this.handleChange}
+                  className="select-element"
+              >
+                  <option value="Ecommerce">Ecommerce</option>
+                  <option value="Scheduling">Scheduling</option>
+                  <option value="Enterprise">Enterprise</option>
+              </select>
             </div>
 
-            <div>
+            <div className="one-column">
                 <textarea
                     type="text"
                     name="description"
@@ -197,27 +197,34 @@ export default class PortfolioForm extends Component {
                     config={this.componentConfig()}
                     djsConfig={this.djsConfig()}
                     eventHandlers={this.handleThumbDrop()}
-                />
+                >
+                  <div className="dz-message">Thumbnail</div>
+                </DropzoneComponent>
 
                 <DropzoneComponent
                     ref={this.bannerRef}
                     config={this.componentConfig()}
                     djsConfig={this.djsConfig()}
                     eventHandlers={this.handleBannerDrop()}
-                />
+                >
+                  <div className="dz-message">Banner</div>
+                </DropzoneComponent>
+                
 
                 <DropzoneComponent
                     ref={this.logoRef}
                     config={this.componentConfig()}
                     djsConfig={this.djsConfig()}
                     eventHandlers={this.handleLogoDrop()}
-                />
+                >
+                  <div className="dz-message">Logo</div>
+                </DropzoneComponent> 
             </div>    
 
           
 
             <div>
-                <button type="submit">Save</button>
+                <button className="btn" type="submit">Save</button>
             </div>
         </form>
       
