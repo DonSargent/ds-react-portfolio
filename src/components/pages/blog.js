5ff619246ae4823084673b8c0a0d7c59fa754640
loganbalck <51034430+loganbalck@users.noreply.github.com>
2021-02-03 12:35:20 -0800

    second commit

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import axios from "axios";
 
-export default function() {
-  return (
-        <div>
-          <h2>Blog</h2>
 
-          <div>
-          <Link to="/about-me">Read About Me</Link>
-          </div>
+class Blog extends Component {
+  constructor() {
+    super();
 
-        </div>
+    this.state = {
+      blogItems: []
+    }
 
-      
-  );
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+      axios.get("https://donsargent.devcamp.space/portfolio/portfolio_blogs", { withCredentials: true 
+    }).then(response => {
+      this.setState({
+        blogItems: response.data.portfolio_blogs
+      });
+    })
+    .catch(error => {
+      console.log("getBlogItems error", error);
+    });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <h1>{blogItem.title}</h1>;
+    });
+    
+    return <div>{blogRecords}</div>;
+  }    
 }
+
+export default Blog
--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -5,6 +5,7 @@ import DropzoneComponent from "react-dropzone-component";
 import "../../../node_modules/react-dropzone-component/styles/filepicker.css";
 import "../../../node_modules/dropzone/dist/min/dropzone.min.css";
 
+
 export default class PortfolioForm extends Component {
   constructor(props) {
     super(props);
@@ -30,12 +31,29 @@ export default class PortfolioForm extends Component {
     this.handleThumbDrop = this.handleThumbDrop.bind(this);
     this.handleBannerDrop = this.handleBannerDrop.bind(this);
     this.handleLogoDrop = this.handleLogoDrop.bind(this);
+    this.deleteImage = this.deleteImage.bind(this);
 
     this.thumbRef = React.createRef();
     this.bannerRef = React.createRef();
     this.logoRef = React.createRef();
   }
 
+  deleteImage(imageType) {
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/delete-portfolio-image/${this.state
+        .id}?image_type=${imageType}`, 
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.setState({
+          [`${imageType}_url`]: ""
+        });
+      })
+      .catch(error => {
+        console.log("deleteImage error", error);
+      });
+    }
   componentDidUpdate() {
     if (Object.keys(this.props.portfolioToEdit).length > 0) {
       const {
@@ -44,7 +62,10 @@ export default class PortfolioForm extends Component {
         description,
         category,
         position,
-        url
+        url,
+        thumb_image_url,
+        banner_image_url,
+        logo_url
       } = this.props.portfolioToEdit;
 
       this.props.clearPortfolioToEdit();
@@ -58,7 +79,10 @@ export default class PortfolioForm extends Component {
         url: url || "",
         editMode: true,
         apiUrl: `https://donsargent.devcamp.space/portfolio/portfolio_items/${id}`,
-        apiAction: "patch"
+        apiAction: "patch",
+        thumb_image_url: thumb_image_url || "",
+        banner_image_url: banner_image_url || "",
+        logo_url: logo_url || ""
       });
     }
   }
@@ -217,15 +241,39 @@ export default class PortfolioForm extends Component {
             />
         </div>
 
-        <div className="image-uploaders">
+            <div className="image-uploaders">
+              {this.state.thumb_image_url && this.state.editMode ? (
+              <div className="portfolio-manager-image-wrapper">
+                <img src={this.state.thumb_image_url} />
+
+                <div className="image-removel-link">
+                  <a onClick={() => this.deleteImage("thumb_image")}>
+                    remove file
+                  </a>
+                </div>  
+              </div>
+             ) : (
             <DropzoneComponent
-                ref={this.componentRef}
-                config={this.componentConfig()}
-                djsConfig={this.djsConfig()}
-                eventHandlers={this.handleThumbDrop()}
+              ref={this.thumbRef}
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleThumbDrop()}
             >
               <div className="dz-message">Thumbnail</div>
             </DropzoneComponent>
+          )}
+
+                      {this.state.banner_image_url && this.state.editMode ? (
+              <div className="portfolio-manager-image-wrapper">
+                <img src={this.state.banner_image_url} />
+
+                <div className="image-removel-link">
+                  <a onClick={() => this.deleteImage("banner_image")}>
+                    remove file
+                  </a>
+                </div>
+              </div>
+              )  : (
 
             <DropzoneComponent
                 ref={this.bannerRef}
@@ -235,7 +283,18 @@ export default class PortfolioForm extends Component {
             >
               <div className="dz-message">Banner</div>
             </DropzoneComponent>
-            
+              )}
+
+              {this.state.logo_url && this.state.editMode ? (
+              <div className="portfolio-manager-image-wrapper">
+                <img src={this.state.logo_url} />
+                <div className="image-removel-link">
+                  <a onClick={() => this.deleteImage("logo")}>
+                    remove file
+                  </a>
+                </div>
+              </div>
+              )  : (
 
             <DropzoneComponent
                 ref={this.logoRef}
@@ -245,6 +304,8 @@ export default class PortfolioForm extends Component {
             >
               <div className="dz-message">Logo</div>
             </DropzoneComponent> 
+              )}
+
         </div>    
         <div>
             <button className="btn" type="submit">Save</button>
