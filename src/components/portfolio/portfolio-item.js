e474b6f3216e13aa975bdc0937c25a83b21dea28
Don Sargent <donsargent@gmail.com>
2021-09-01 13:46:39 -0700

    second commit

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
-import { Link } from  'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-
-export default class Portfolio extends Component   {
+export default class PortfolioItem extends Component {
   constructor(props) {
     super(props);
 
@@ -12,37 +11,34 @@ export default class Portfolio extends Component   {
   }
 
   handleMouseEnter() {
-    this.setState({ portfolioItemClass: "image-blur"});
+    this.setState({ portfolioItemClass: "image-blur" });
   }
 
   handleMouseLeave() {
-    this.setState({ portfolioItemClass: ""});
+    this.setState({ portfolioItemClass: "" });
   }
-  
-  render(){
+
+  render() {
     const { id, description, thumb_image_url, logo_url } = this.props.item;
     return (
       <Link to={`/portfolio/${id}`}>
-        <div 
+        <div
           className="portfolio-item-wrapper"
           onMouseEnter={() => this.handleMouseEnter()}
           onMouseLeave={() => this.handleMouseLeave()}
         >
-        
-          
-          <div 
+          <div
             className={
               "portfolio-img-background " + this.state.portfolioItemClass
             }
             style={{
-              backgroundImage: "url(" + thumb_image_url + ")" 
-            }} 
+              backgroundImage: "url(" + thumb_image_url + ")"
+            }}
           />
-          
+
           <div className="img-text-wrapper">
-            
             <div className="logo-wrapper">
-              <img scr={logo_url} />
+              <img src={logo_url} />
             </div>
 
             <div className="subtitle">{description}</div>
